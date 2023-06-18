02f608e5602d04fccfbe2294df9ebfe2bb73ae92
Tarasdeveloper <tomahavki1@gmail.com>
2023-06-18 14:10:31 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import user from '../../data/user.json';
 import data from '../../data/data.json';
-import friends from '../../data/friends.json';
-import transactions from '../../data/transactions.json';
+// import friends from '../../data/friends.json';
+// import transactions from '../../data/transactions.json';
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics.styled';
 
